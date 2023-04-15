03981ea3e528efadc7fc8a11ad09a6d835cd3546
Conner McGuire <connerpmcguire@gmail.com>
2023-04-15 13:59:14 -0500

    gs

--- src/components/Places/PlacesList.tsx
+++ src/components/Places/PlacesList.tsx
@@ -1,27 +1,25 @@
 import { useEffect, useState } from "react";
 import { getAllPlaces } from "../../services/places";
 import { Place } from "../../interfaces/Places";
+import { useQuery } from "@tanstack/react-query";
 
 // todo use react query to fetch
+// - [x] typescript
+// - [x] devtools
 
 export const PlacesList = () => {
-  const [places, setPlaces] = useState<Place[]>([]);
-  // todo call service function to getAllPlaces
-  useEffect(() => {
-    const fetchPlaces = async () => {
-      // todo fetch from service
-      const data = await getAllPlaces();
-      setPlaces(data);
-    };
-
-    fetchPlaces();
-  }, []);
+  const { isLoading, isError, data, error } = useQuery<Place[], Error>({
+    queryKey: ["places"],
+    queryFn: getAllPlaces,
+  });
 
   return (
     <div>
       <h1>Places List</h1>
       <ul>
-        {places?.map((place) => (
+        {isLoading && <p>loading...</p>}
+        {isError && <p>Error: {error.toString()}</p>}
+        {data?.map((place) => (
           <li key={place._id}>{place.name}</li>
         ))}
       </ul>
