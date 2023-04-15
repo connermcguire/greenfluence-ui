9b1989d777e8c30966edd1ba00d4f80a5d0413ca
Conner McGuire <connerpmcguire@gmail.com>
2023-04-15 14:13:43 -0500

    add redirects

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
-import { Link, RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  Link,
+  RouterProvider,
+  createBrowserRouter,
+  // Routes,
+  // Route,
+  // createRoutesFromElements,
+} from "react-router-dom";
 import HelloWorld from "./HelloWorld";
 import PlacesPage from "./pages/Places";
+
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Routes path="/" element={<></>}>
+//       <Route index element={<HelloWorld />} />
+//       <Route path="places" element={<PlacesPage />} />
+//       <Route
+//         path="hello"
+//         element={
+//           <div>
+//             <h1>Hello World</h1>
+//             <Link to="/">Back to Home</Link>
+//           </div>
+//         }
+//       />
+//     </Routes>
+//   )
+// );
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HelloWorld />,
+    // errorElement: <ErrorPage />,
   },
   {
     path: "places",
